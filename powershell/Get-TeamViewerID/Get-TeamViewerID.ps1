09bfa1a139290ac2061c5578271a4d1e4d3480a7
Diskoteket <timwa@protonmail.com>
2020-08-10 09:31:17 +0200

    editing description

--- powershell/Get-TeamViewerID/Get-TeamViewerID.ps1
+++ powershell/Get-TeamViewerID/Get-TeamViewerID.ps1
@@ -1,10 +1,10 @@
 <#
 .Synopsis
-    This script gets the ID used in connecting to remote computers with Team Viewer
+    This script gets the ID used in connecting to remote computers with TeamViewer
 
 
 .DESCRIPTION
-    This is a small script mainly used by our serivcedesk to get the Team Viewer ID of computers in our organization.
+    This is a small script mainly used by our serivcedesk to get the TeamViewer ID of computers in our organization.
     The reason why this script is needed is because sometimes the agents can't connect to a computer with just the host name.
     So instead of getting the user to read the ID out load you can simply remote to the computer and get the ID.
 
